f2932e17a64a8e0f36ed3c1710ee455cd4359c4e
Liz Wong <anotherlizwong@gmail.com>
2015-05-16 02:27:10 -0400

    fine whatever.

--- scripts/cap.drag.js
+++ scripts/cap.drag.js
@@ -1,13 +1,22 @@
 jQuery(function ($) {
-   var z = 1;
    $('.drag')
-   .drag("start",function () {
-      $(this).css('zIndex', z++);
+   .drag("start", function () {
+      return $(this).clone()
+      .css("opacity", 0.75)
+      .insertAfter($(this));
    })
-   .drag(function(ev, dd) {
-      $(this).css({
+   .drag(function (ev, dd){
+      $(dd.proxy).css({
          top: dd.offsetY,
          left: dd.offsetX
       });
+   })
+   .drag("end", function (ev, dd) {
+      // $(this).animate({
+      //    top: dd.offsetY,
+      //    left: dd.offsetX
+      // }, 420);
+      $(dd.proxy).remove();
+
    });
 });
